e6039aa0d6d33ff0653a1d157a20a534ff9079b3
Chris Cummins <chrisc.101@gmail.com>
2018-06-13 22:53:42 +0000

    Add shell aliases for docker.
    [Exported from b0b28ea3d0296078da02e4dc141c30cee2382184]

--- system/dotfiles/usr/share/Zsh/docker.zsh
+++ system/dotfiles/usr/share/Zsh/docker.zsh
@@ -1,20 +1,6 @@
 # Aliases and configuration for Docker.
 
 alias d='docker'
-alias dp='docker push'
 alias dls='docker image ls'
 alias drm='docker rmi'
 alias dpr='docker system prune'
-
-dtp() {
-  docker tag $1 $2;
-  docker push $2;
-}
-
-# Usage: <bazel_target> <docker_label>
-docker_bazel_push() {
-  bazel build //$1.tar;
-  docker load -i bazel-bin/$1.tar;
-  docker tag bazel/$1 $2;
-  docker push $2;
-}

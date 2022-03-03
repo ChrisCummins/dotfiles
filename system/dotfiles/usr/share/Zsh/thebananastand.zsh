986d3cb114bbfc47992c70c096ab417e94ed3a12
Chris Cummins <cummins@fb.com>
2022-03-03 14:20:46 +0000

    [zsh] Fix alias.

--- system/dotfiles/usr/share/Zsh/thebananastand.zsh
+++ system/dotfiles/usr/share/Zsh/thebananastand.zsh
@@ -1,2 +1,2 @@
 alias tbs-on='wakeonlan 90:09:D0:06:3F:9F'
-alias tbs-ssh="cummins@TheBananaStand.local -t '/usr/local/bin/tmux -u -CC new -A -s main'"
+alias tbs-ssh="ssh cummins@TheBananaStand.local -t '/usr/local/bin/tmux -u -CC new -A -s main'"
